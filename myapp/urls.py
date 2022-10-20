6ec805801f6487b31ece13e1cb9fb772be975f22
alby <albyelizabeth2001@gmail.com>
2022-10-20 19:27:08 +0530

    third commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,11 @@
 
 
 from django.urls import path,include
-from .views import index,new_one
+from .views import index,new_one,products,product_details,add_product
 urlpatterns = [
     path('',index),
-    path('new',new_one,)
+    path('new/',new_one),
+    path('products/',products,name='products'),
+    path('products/<int:id>',product_details,name='product_details'),
+    path('product/add',add_product,name='add_product'),
 ] 
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,9 @@
 from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
+from myapp.models import Product
 
-# Create your views here.
+# Create your views here.-
 
 
 def index(request):
@@ -17,7 +18,7 @@ def index(request):
     for i in range(0,10):
       print(i)
       
-    context = {'names':li}
+    context = {'li':li}
     
     
     return render(request, 'myapp/index.html',context=context)
@@ -25,3 +26,26 @@ def index(request):
 
 def new_one(request):
     return render(request, 'listing/new_one.html')
+
+def products(request):
+    p = Product.objects.all()
+    
+    context = {'products':p}
+    
+    return render(request, 'myapp/products.html',context=context)
+
+
+def product_details(request,id):
+    p = Product.objects.get(id=id)
+    context = {'p':p}
+    return render(request, 'myapp/product_details.html',context=context)
+
+
+def add_product(request):
+    p = Product(name = "Samsung 32 Inch Monitor",price = 36000.0)
+    
+    p.description = "This is a Samsung 32 Inch Monitor"
+
+    p.save()
+    
+    return HttpResponse(p)
